699e76166fa6410bf3755379380e3d5b6b2ef2be
yustik <yustik74@yandex.ru>
2021-02-16 17:43:34 +0500

    fix: исправил ошибку с выводом рассчетного расстояния в таблице, исправил ошибки с выдачей результата из Promise в graphhopper

--- AppTemplates/routeCompare/graphhopper.js
+++ AppTemplates/routeCompare/graphhopper.js
@@ -24,7 +24,7 @@ class GraphhopperApi {
                         result.points.push([point[1], point[0]]);
                     }
                 );
-                return result;
+                resolve(result);
             });
         });
     };
