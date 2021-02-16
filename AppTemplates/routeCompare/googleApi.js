2b1dd01176c7c6f0405338aaf190261809bd7405
yustik <yustik74@yandex.ru>
2021-02-16 16:31:24 +0500

    feat: разнес обработчики по отдельным модулям, вынес веб в отдельный модуль, сделал запрос и заполнение маршрута из метода пуеКщгеу обработчика, обработчик выбирается из dropDownList

---
+++ AppTemplates/routeCompare/googleApi.js
@@ -0,0 +1,30 @@
+class GoogleApi {
+
+    constructor (parm)
+    {
+        this.parm = parm;
+    }
+
+    getName = 'Google API';
+
+    getRoute = function (startPoint, endPoint) {
+        get(parm.Settings['graphhopper_url'] + '/route', {
+            key: parm.Settings['graphhopper_api_key'],
+            vehicle: 'car',
+            type: 'json',
+            point: [pointToParameterFormat(startPoint), pointToParameterFormat(endPoint)],
+            points_encoded: 'false',
+            locale: 'ru',
+            instructions: 'false'
+        }, function (route) {
+            var result = Object();
+            result.totalDistance = round(route.paths[0].distance / 1000);
+            result.points = [];
+            route.paths[0].points.coordinates.forEach(function (point) {
+                    result.points.push([point[1], point[0]]);
+                }
+            );
+            return result;
+        });
+    };
+}
