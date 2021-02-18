42505b54ff3298905b4885ebb56a67f7c6c95e13
yustik <yustik74@yandex.ru>
2021-02-18 16:05:56 +0500

    feat: координаты через cellTemplate

--- AppTemplates/detailTemplate/apiassistant.js
+++ AppTemplates/detailTemplate/apiassistant.js
@@ -21,20 +21,22 @@ function round(value, digits = 2) {
 
 //перевод UTC-даты в формат строки для API
 function dateToUtcApiFormat(date) {
-    var twoDigitMonth = ((date.getUTCMonth() + 1) >= 10) ? (date.getUTCMonth() + 1) : '0' + (date.getUTCMonth() + 1);
-    var twoDigitDate = ((date.getUTCDate()) >= 10) ? (date.getUTCDate()) : '0' + (date.getUTCDate());
-    var twoDigitHours = ((date.getUTCHours()) >= 10) ? (date.getUTCHours()) : '0' + (date.getUTCHours());
-    var twoDigitMinutes = ((date.getUTCMinutes()) >= 10) ? (date.getUTCMinutes()) : '0' + (date.getUTCMinutes());
-    return date.getUTCFullYear() + twoDigitMonth + twoDigitDate + '-' + (twoDigitHours) + twoDigitMinutes;
+    let twoDigitMonth = ((date.getUTCMonth() + 1) >= 10) ? (date.getUTCMonth() + 1) : '0' + (date.getUTCMonth() + 1);
+    let twoDigitDate = ((date.getUTCDate()) >= 10) ? (date.getUTCDate()) : '0' + (date.getUTCDate());
+    let twoDigitHours = ((date.getUTCHours()) >= 10) ? (date.getUTCHours()) : '0' + (date.getUTCHours());
+    let twoDigitMinutes = ((date.getUTCMinutes()) >= 10) ? (date.getUTCMinutes()) : '0' + (date.getUTCMinutes());
+    let twoDigitSeconds = ((date.getUTCSeconds()) >= 10) ? (date.getUTCSeconds()) : '0' + (date.getUTCSeconds());
+    return date.getUTCFullYear() + twoDigitMonth + twoDigitDate + '-' + (twoDigitHours) + twoDigitMinutes + twoDigitSeconds;
 }
 
 //перевод даты в формат строки для API
 function dateToApiFormat(date) {
-    var twoDigitMonth = ((date.getMonth() + 1) >= 10) ? (date.getMonth() + 1) : '0' + (date.getMonth() + 1);
-    var twoDigitDate = ((date.getDate()) >= 10) ? (date.getDate()) : '0' + (date.getDate());
-    var twoDigitHours = ((date.getHours()) >= 10) ? (date.getHours()) : '0' + (date.getHours());
-    var twoDigitMinutes = ((date.getMinutes()) >= 10) ? (date.getMinutes()) : '0' + (date.getMinutes());
-    return date.getFullYear() + twoDigitMonth + twoDigitDate + '-' + (twoDigitHours) + twoDigitMinutes;
+    let twoDigitMonth = ((date.getMonth() + 1) >= 10) ? (date.getMonth() + 1) : '0' + (date.getMonth() + 1);
+    let twoDigitDate = ((date.getDate()) >= 10) ? (date.getDate()) : '0' + (date.getDate());
+    let twoDigitHours = ((date.getHours()) >= 10) ? (date.getHours()) : '0' + (date.getHours());
+    let twoDigitMinutes = ((date.getMinutes()) >= 10) ? (date.getMinutes()) : '0' + (date.getMinutes());
+    let twoDigitSeconds = ((date.getSeconds()) >= 10) ? (date.getSeconds()) : '0' + (date.getSeconds());
+    return date.getFullYear() + twoDigitMonth + twoDigitDate + '-' + (twoDigitHours) + twoDigitMinutes + twoDigitSeconds;
 }
 
 function pointToParameterFormat(point) {
@@ -42,8 +44,7 @@ function pointToParameterFormat(point) {
 }
 
 //получение массива точек трека из объекта трека, полученного по GetTrack
-function getPointsFromTrack(carId, track)
-{
+function getPointsFromTrack(carId, track) {
     let points = [];
     for (let counter = 0; counter < track[carId][0].DT.length; counter++) {
         points.push([track[carId][0].Lat[counter], track[carId][0].Lng[counter]]);
@@ -51,16 +52,14 @@ function getPointsFromTrack(carId, track)
     return points;
 }
 
-function setStartOfTheDay(date)
-{
+function setStartOfTheDay(date) {
     date.setHours(0);
     date.setMinutes(0);
     date.setSeconds(0);
     return date;
 }
 
-function setEndOfTheDay(date)
-{
+function setEndOfTheDay(date) {
     date.setHours(23);
     date.setMinutes(59);
     date.setSeconds(59);
