b2d09886b68ef09b435d08aa88a90aa0865ab775
yustik <yustik74@yandex.ru>
2021-02-25 12:05:10 +0500

    fix: refactoring

--- AppTemplates/routeCompare/js/apiassistant.js
+++ AppTemplates/routeCompare/js/apiassistant.js
@@ -19,24 +19,6 @@ function round(value, digits = 2) {
     return Math.round((value) * multiplier) / multiplier;
 }
 
-//перевод UTC-даты в формат строки для API
-function dateToUtcApiFormat(date) {
-    var twoDigitMonth = ((date.getUTCMonth() + 1) >= 10) ? (date.getUTCMonth() + 1) : '0' + (date.getUTCMonth() + 1);
-    var twoDigitDate = ((date.getUTCDate()) >= 10) ? (date.getUTCDate()) : '0' + (date.getUTCDate());
-    var twoDigitHours = ((date.getUTCHours()) >= 10) ? (date.getUTCHours()) : '0' + (date.getUTCHours());
-    var twoDigitMinutes = ((date.getUTCMinutes()) >= 10) ? (date.getUTCMinutes()) : '0' + (date.getUTCMinutes());
-    return date.getUTCFullYear() + twoDigitMonth + twoDigitDate + '-' + (twoDigitHours) + twoDigitMinutes;
-}
-
-//перевод даты в формат строки для API
-function dateToApiFormat(date) {
-    var twoDigitMonth = ((date.getMonth() + 1) >= 10) ? (date.getMonth() + 1) : '0' + (date.getMonth() + 1);
-    var twoDigitDate = ((date.getDate()) >= 10) ? (date.getDate()) : '0' + (date.getDate());
-    var twoDigitHours = ((date.getHours()) >= 10) ? (date.getHours()) : '0' + (date.getHours());
-    var twoDigitMinutes = ((date.getMinutes()) >= 10) ? (date.getMinutes()) : '0' + (date.getMinutes());
-    return date.getFullYear() + twoDigitMonth + twoDigitDate + '-' + (twoDigitHours) + twoDigitMinutes;
-}
-
 function pointToParameterFormat(point) {
     return point.Lat + ',' + point.Lng;
 }
