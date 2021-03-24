3f73b07fccb061bb86fb899dea3db82025848253
yustik <yustik74@yandex.ru>
2021-03-24 11:49:57 +0500

    fix: рефакторинг

--- AppTemplates/CityService/js/map.js
+++ AppTemplates/CityService/js/map.js
@@ -58,144 +58,165 @@ function InitMap(id, maxClusterRadius, defaultView) {
 }
 
 function createMarker(item) {
-    return new Promise((resolve) => {
-        let info = item.Name + " " + item.Plate + "<br>Скорость: " + round(item.Speed, 2) + "км/ч<br>Состояние: " + getStateName(item.Speed) + "<br>Последняя активность: " + getTimeDifferenceString(item.LastTime);
+    return new Promise(async (resolve) => {
+        let info = formatMarkerInfo(item);
         item.Alias = info;
         item.showLabel = false;
         let canvasElement = document.createElement('canvas');
-        drawCursor(canvasElement, item).then(function (image) {
-            let marker = L.marker([item.LastPosition.Lat, item.LastPosition.Lng], {
-                icon: L.icon({
-                    iconUrl: image.image,
-                    iconAnchor: [image.anchorX, image.anchorY],
-                })
-            });
-            //marker.bindPopup(info);
-            marker.bindTooltip(info);
-            resolve(marker);
-        });
-    });
-}
-
-function updateMarker(item)
-{
-    return new Promise((resolve) => {
-        let info = item.Name + " " + item.Plate + "<br>Скорость: " + round(item.Speed, 2) + "км/ч<br>Состояние: " + getStateName(item.Speed) + "<br>Последняя активность: " + getTimeDifferenceString(item.LastTime);
-        item.Alias = info;
-        item.showLabel = false;
-        let canvasElement = document.createElement('canvas');
-        drawCursor(canvasElement, item).then(function (image) {
-            item.Marker.setIcon(L.icon({
-                    iconUrl: image.image,
-                    iconAnchor: [image.anchorX, image.anchorY],
-                }));
-            item.Marker.bindTooltip(info);
-            item.Marker.setLatLng([item.LastPosition.Lat, item.LastPosition.Lng]);
-            resolve(item.Marker);
-        });
-    });
-}
-
-function drawCursor(canvasElement, car) {
-
-    canvasElement.width = 48;
-    canvasElement.height = 96;
-    let context = canvasElement.getContext('2d');
-    let xPos = canvasElement.width / 2;
-    let yPos = canvasElement.height / 4 * 3;
-    if (markerType == 0)
-        return new Promise((resolve) => {
-            getDefaultArrowCursor(canvasElement.width, car, cursorColor).then(function (image) {
-                context.drawImage(image.image, 0, canvasElement.height / 2);
-                xPos = image.anchorX;
-                yPos = image.anchorY + canvasElement.height / 2;
-                getIcon(canvasElement.width, car.IconPath).then(function (imageCar) {
-                    context.drawImage(imageCar, 0, canvasElement.height / 2);
-                    resolve({image: canvasElement.toDataURL(), anchorX: xPos, anchorY: yPos});
-                });
-            });
-        });
-
-    return new Promise((resolve) => {
-        new Promise((resolve) => {
-            if (flagIsSet(cursorSet) && parm.Settings['CursorPath'] != none) {
-                if (parm.Settings['CursorPath'] == auto) {
-                    getDefaultArrowCursor(canvasElement.width, car, cursorColor).then(function (image) {
-                        xPos = image.anchorX;
-                        yPos = image.anchorY + canvasElement.height / 2;
-                        context.drawImage(image.image, 0, canvasElement.height / 2);
-                        resolve();
-                    });
-                } else {
-                    getImage(parm.Settings['CursorPath']).then(function (image) {
-                        let cursorCanvasElement = document.createElement('canvas');
-                        cursorCanvasElement.width = canvasElement.width;
-                        cursorCanvasElement.height = canvasElement.height / 2;
-                        let offsetWidth = cursorCanvasElement.width / 2;
-                        let offsetHeight = cursorCanvasElement.height / 2;
-                        let cursorContext = cursorCanvasElement.getContext('2d');
-                        cursorContext.translate(offsetWidth, offsetHeight);
-                        cursorContext.rotate(calculateRadian(car.Course));
-                        cursorContext.drawImage(image, -offsetWidth, -offsetHeight, cursorCanvasElement.width, cursorCanvasElement.height);
-                        context.drawImage(cursorCanvasElement, 0, canvasElement.height / 2);
-                        resolve();
-                    });
-                }
-            } else
-                resolve();
-
-        }).then(function () {
-            return new Promise((resolve) => {
-                if (flagIsSet(cursorBackgroundSet) && parm.Settings['CursorBackgroundPath'] != none) {
-                    if (parm.Settings['CursorBackgroundPath'] == auto) {
-                        getIcon(canvasElement.width, car.IconPath).then(function (imageCar) {
-                            context.drawImage(imageCar, 0, canvasElement.height / 2);
-                            resolve();
-                        });
-                    } else
-                        getImage(parm.Settings['CursorBackgroundPath']).then(function (image) {
-                            context.drawImage(image, 0, canvasElement.height / 2);
-                            resolve();
-                        });
-                } else
-                    resolve();
+        let image = await getMarkerImage(canvasElement, item);
+        let marker = L.marker([item.LastPosition.Lat, item.LastPosition.Lng], {
+            icon: L.icon({
+                iconUrl: image.image,
+                iconAnchor: [image.anchorX, image.anchorY],
             })
-        }).then(function () {
-            return new Promise((resolve) => {
-                if (flagIsSet(iconBackgroundSet) && parm.Settings['IconBackgroundPath'] != none) {
-                    if (parm.Settings['IconBackgroundPath'] == auto) {
-                        getDefaultIconBackground(canvasElement.width).then(function (background) {
-                            context.drawImage(background, canvasElement.width * 0.175, 0);
-                            resolve();
-                        });
-                    } else
-                        getImage(parm.Settings['IconBackgroundPath']).then(function (image) {
-                            context.drawImage(image, 0, 0, canvasElement.width, canvasElement.width);
-                            resolve();
-                        });
-                } else
-                    resolve();
-            }).then(function () {
-                return new Promise((resolve) => {
-                    if (flagIsSet(iconSet) && parm.Settings['IconPath'] != none) {
-                        if (parm.Settings['IconPath'] == auto) {
-                            getIcon(canvasElement.width, car.IconPath, 3).then(function (image) {
-                                context.drawImage(image, 0, 0, canvasElement.width, canvasElement.width);
-                                resolve();
-                            });
-                        } else
-                            getImage(parm.Settings['IconPath']).then(function (image) {
-                                context.drawImage(image, 0, 0, canvasElement.width, canvasElement.width);
-                                resolve();
-                            });
-                    } else
-                        resolve();
-                }).then(function () {
-                    resolve({image: canvasElement.toDataURL(), anchorX: xPos, anchorY: yPos});
-                });
-            });
         });
+        marker.bindTooltip(info);
+        resolve(marker);
     });
+}
+
+function updateMarker(item) {
+    return new Promise(async (resolve) => {
+        let info = formatMarkerInfo(item);
+        item.Alias = info;
+        item.showLabel = false;
+        let canvasElement = document.createElement('canvas');
+        let image = await getMarkerImage(canvasElement, item);
+        item.Marker.setIcon(L.icon({
+            iconUrl: image.image,
+            iconAnchor: [image.anchorX, image.anchorY],
+        }));
+        item.Marker.bindTooltip(info);
+        item.Marker.setLatLng([item.LastPosition.Lat, item.LastPosition.Lng]);
+        resolve(item.Marker);
+    });
+}
+
+function formatMarkerInfo(item) {
+    return item.Name + " " + item.Plate + "<br>Скорость: " + round(item.Speed, 2) + "км/ч<br>Состояние: " + getStateName(item.Speed) + "<br>Последняя активность: " + getTimeDifferenceString(item.LastTime);
+}
+
+function getMarkerImage(canvasElement, car) {
+    return new Promise(async (resolve) => {
+        canvasElement.width = 48;
+        canvasElement.height = 96;
+        let context = canvasElement.getContext('2d');
+        let xPos = canvasElement.width / 2;
+        let yPos = canvasElement.height / 4 * 3;
+        if (markerType == 0) {
+            let anchor = await drawDefaultCursor(canvasElement, context, car);
+            xPos = anchor[0];
+            yPos = anchor[1];
+        } else {
+
+            await new Promise(async (resolve) => {
+                let anchor = await drawCursorIcon(canvasElement, context, car);
+                await drawCursorBackground(canvasElement, context, car);
+                await drawIconBackground(canvasElement, context);
+                await drawIcon(canvasElement, context, car);
+                if (anchor) {
+                    xPos = anchor[0];
+                    yPos = anchor[1];
+                }
+                resolve();
+            });
+        }
+        resolve({image: canvasElement.toDataURL(), anchorX: xPos, anchorY: yPos});
+    });
+}
+
+function drawDefaultCursor(canvasElement, context, car) {
+    let xPos = 0;
+    let yPos = 0;
+    return new Promise(async (resolve) => {
+        let image = await getDefaultArrowCursor(canvasElement.width, car, cursorColor);
+        context.drawImage(image.image, 0, canvasElement.height / 2);
+        xPos = image.anchorX;
+        yPos = image.anchorY + canvasElement.height / 2;
+        let imageCar = await getIcon(canvasElement.width, car.IconPath);
+        context.drawImage(imageCar, 0, canvasElement.height / 2);
+        resolve([xPos, yPos]);
+    });
+}
+
+function drawCursorIcon(canvasElement, context, car) {
+    let xPos = 0;
+    let yPos = 0;
+    return new Promise(async (resolve) => {
+        if (flagIsSet(cursorSet) && parm.Settings['CursorPath'] != none) {
+            let cursorCanvasElement;
+            if (parm.Settings['CursorPath'] == auto) {
+                let defaultCursor = await getDefaultArrowCursor(canvasElement.width, car, cursorColor);
+                xPos = defaultCursor.anchorX;
+                yPos = defaultCursor.anchorY + canvasElement.height / 2;
+                cursorCanvasElement = defaultCursor.image;
+            } else {
+                let image = await getImage(parm.Settings['CursorPath']);
+                cursorCanvasElement = drawRotatedCursor(image, canvasElement.width, canvasElement.height / 2, car.Course);
+            }
+            context.drawImage(cursorCanvasElement, 0, canvasElement.height / 2);
+            resolve([xPos, yPos]);
+        } else
+            resolve();
+    });
+}
+
+function drawCursorBackground(canvasElement, context, car) {
+    return new Promise(async (resolve) => {
+        if (flagIsSet(cursorBackgroundSet) && parm.Settings['CursorBackgroundPath'] != none) {
+            let image;
+            if (parm.Settings['CursorBackgroundPath'] == auto)
+                image = await getIcon(canvasElement.width, car.IconPath);
+            else
+                image = await getImage(parm.Settings['CursorBackgroundPath']);
+            context.drawImage(image, 0, canvasElement.height / 2, canvasElement.width, canvasElement.height / 2);
+            resolve();
+        } else
+            resolve();
+    });
+}
+
+function drawIconBackground(canvasElement, context) {
+    return new Promise(async (resolve) => {
+        if (flagIsSet(iconBackgroundSet) && parm.Settings['IconBackgroundPath'] != none) {
+            if (parm.Settings['IconBackgroundPath'] == auto) {
+                let background = await getDefaultIconBackground(canvasElement.width);
+                context.drawImage(background, canvasElement.width * 0.175, 0, canvasElement.width, canvasElement.height / 2);
+            } else {
+                let image = await getImage(parm.Settings['IconBackgroundPath']);
+                context.drawImage(image, 0, 0, canvasElement.width, canvasElement.height / 2);
+            }
+            resolve();
+        } else
+            resolve();
+    });
+}
+
+function drawIcon(canvasElement, context, car) {
+    return new Promise(async (resolve) => {
+        if (flagIsSet(iconSet) && parm.Settings['IconPath'] != none) {
+            let image;
+            if (parm.Settings['IconPath'] == auto) {
+                image = await getIcon(canvasElement.width, car.IconPath, 3)
+            } else
+                image = await getImage(parm.Settings['IconPath']);
+            context.drawImage(image, 0, 0, canvasElement.width, canvasElement.height / 2);
+            resolve();
+        } else
+            resolve();
+    });
+}
+
+function drawRotatedCursor(image, width, height, angle) {
+    let cursorCanvasElement = document.createElement('canvas');
+    cursorCanvasElement.width = width;
+    cursorCanvasElement.height = height;
+    let offsetWidth = cursorCanvasElement.width / 2;
+    let offsetHeight = cursorCanvasElement.height / 2;
+    let cursorContext = cursorCanvasElement.getContext('2d');
+    cursorContext.translate(offsetWidth, offsetHeight);
+    cursorContext.rotate(calculateRadian(angle));
+    cursorContext.drawImage(image, -offsetWidth, -offsetHeight, cursorCanvasElement.width, cursorCanvasElement.height);
 }
 
 function flagIsSet(flag) {
