398959e795864a50fc638b758a1570d27662fe0f
yustik <yustik74@yandex.ru>
2021-03-25 15:30:21 +0500

    fix: адаптивная верстка формы, добавлена возможность выключать блоки на форме

--- AppTemplates/CityService/js/map.js
+++ AppTemplates/CityService/js/map.js
@@ -34,17 +34,18 @@ function InitMap(id, maxClusterRadius, defaultView) {
     let mapObject = Object();
     mapObject.map = L.map(id, {
         preferCanvas: true,
+        attributionControl: false,
     }).setView(coords, 13);
+    mapObject.map.zoomControl.setPosition('bottomright');
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 18,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    })
-        .addTo(mapObject.map);
+        maxZoom: 18
+    }).addTo(mapObject.map);
     if (!!!maxClusterRadius)
         maxClusterRadius = 120;
     mapObject.devicesLayer = L.markerClusterGroup({
         maxClusterRadius: maxClusterRadius,
+        showCoverageOnHover: false,
         iconCreateFunction: function (cluster) {
             let count = cluster.getAllChildMarkers().length;
             return L.divIcon({
@@ -93,5 +94,8 @@ function updateMarker(item) {
 }
 
 function formatMarkerInfo(item) {
-    return item.Name + " " + item.Plate + "<br>Скорость: " + round(item.Speed, 2) + "км/ч<br>Состояние: " + getStateName(item.Speed) + "<br>Последняя активность: " + getTimeDifferenceString(item.LastTime);
+    return "<b>" + item.Name + "</b>"
+        + " " + item.Plate
+        + "<br>Скорость: " + round(item.Speed, 2) + " км/ч<br>Состояние: " + getStateName(item.Speed)
+        + "<br>Последняя активность: " + getTimeDifferenceString(item.LastTime);
 }
